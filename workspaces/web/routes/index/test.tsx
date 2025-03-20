ed02d2409ac886cb4c4a7578492badea8b3c7ce8
Andrew Shaw Care <andrewshawcare@gmail.com>
2025-03-20 12:13:59 -0400

    Refactor tests for IndexRoute to use createRoot and remove testRender utility

--- workspaces/web/routes/index/test.tsx
+++ workspaces/web/routes/index/test.tsx
@@ -1,10 +1,18 @@
+import "global-jsdom/register";
 import test from "node:test";
 import assert from "node:assert";
 import { IndexRoute } from "./route";
-import { testRender } from "../../utilities/test-render";
+import { act } from "react";
+import { createRoot } from "react-dom/client";
 
 test("includes the appropriate text content", async () => {
-  const rootElement = await testRender(<IndexRoute path="/" />);
+  const rootElement = document.createElement("main");
+  document.body.appendChild(rootElement);
 
-  assert.strictEqual(rootElement.textContent, "Index");
+  global.IS_REACT_ACT_ENVIRONMENT = true;
+  await act(() => {
+    createRoot(rootElement).render(<IndexRoute path="/" />);
+  });
+
+  assert.strictEqual(rootElement.textContent, "");
 });
