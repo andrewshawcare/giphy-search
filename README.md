10acbab674ba7242caa5c84e22932e9adaee57b1
Andrew Shaw Care <andrewshawcare@gmail.com>
2025-03-20 21:32:38 -0400

    Update README with setup instructions, modify package.json scripts, and enhance Giphy search response handling

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # gotcare
+
+## Getting started
+
+### Enviornment variables
+
+Ensure you add `.env` files to each workspace, using the appropriate `.env.template` file as a base:
+
+```sh
+for dir in "./workspaces/*"; do (cd "${dir} && cp -n .env.template .env); done
+```
+
+### Build
+
+```sh
+npm run build
+```
+
+### Lint
+
+```sh
+npm run lint
+```
+
+### Test
+
+```sh
+npm run test
+```
+
+### Develop
+
+```sh
+npm run develop
+```
--- workspaces/api/src/routers/search/index.ts
+++ workspaces/api/src/routers/search/index.ts
@@ -2,30 +2,59 @@ import querystring from "node:querystring";
 import Express from "express";
 import { configuration } from "../../configuration.js";
 
+interface GiphyImage {
+  url: string;
+}
+
+interface GiphyImageFormats {
+  fixed_width: GiphyImage;
+}
+
+interface GiphySearchDatum {
+  import_datetime: string;
+  images: GiphyImageFormats;
+}
+
+interface GiphySearchResponse {
+  data: GiphySearchDatum[];
+}
+
 const router = Express.Router();
+
 router.get("/:q", async (req, res) => {
   const qs = querystring.stringify({
     api_key: configuration.giphy.key,
     q: req.params.q,
   });
+
   const response = await fetch(
     `${configuration.giphy.origin}/v1/gifs/search?${qs}`
   );
-  const json = await response.json();
-  const images = json.data
-    .map((datum: any) => {
-      const importDateTime = datum.import_datetime;
-      const url = datum.images.fixed_width.url;
 
-      return { importDateTime, url };
-    })
-    .reduce((images: any, image: any) => {
-      const { importDateTime, url } = image;
-      const year = new Date(importDateTime).getFullYear();
+  const giphySearchResponse: GiphySearchResponse = await response.json();
+
+  const images = giphySearchResponse.data
+    .map(
+      ({
+        import_datetime,
+        images: {
+          fixed_width: { url },
+        },
+      }) => {
+        return { import_datetime, url };
+      }
+    )
+    .reduce((images: { [key: number]: string[] | undefined }, image) => {
+      const { import_datetime, url } = image;
+
+      const year = new Date(import_datetime).getFullYear();
+
       images[year] = images[year] || [];
-      images[year].push(image.url);
+      images[year].push(url);
+
       return images;
     }, {});
+
   res.status(200).send(images);
 });
 
