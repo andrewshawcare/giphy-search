9e557e25b0d24efa5a15beb715a4661b9c617fad
Andrew Shaw Care <andrewshawcare@gmail.com>
2025-03-21 16:01:02 -0400

    Add WEB_ORIGIN configuration and update CORS middleware to use it

--- workspaces/api/src/app.ts
+++ workspaces/api/src/app.ts
@@ -1,17 +1,12 @@
 import CORS from "cors";
 import Express from "express";
 import SearchRouter from "./router/search/index.js";
+import { configuration } from "./configuration.js";
 
 export const app = Express();
 
 app.use(Express.static("static"));
 
-app.use(
-  CORS({
-    origin(requestOrigin, callback) {
-      callback(null, requestOrigin);
-    },
-  })
-);
+app.use(CORS({ origin: configuration.web.origin }));
 
 app.use("/api/search", SearchRouter);
--- workspaces/api/src/configuration.ts
+++ workspaces/api/src/configuration.ts
@@ -3,6 +3,9 @@ export interface Configuration {
     origin: string;
     key: string;
   };
+  web: {
+    origin: string;
+  };
   api: {
     port: number;
   };
@@ -12,6 +15,9 @@ export const configuration: Configuration = {
   giphy: {
     origin: process.env["GIPHY_ORIGIN"]!,
     key: process.env["GIPHY_KEY"]!,
+  },
+  web: {
+    origin: process.env["WEB_ORIGIN"]!,
   },
   api: {
     port: parseInt(process.env["PORT"]!, 10),
