5ef4358da03b38a0bf3b395595e0c08aa5e12531
Andrew Shaw Care <andrewshawcare@gmail.com>
2025-03-21 15:47:47 -0400

    Add image dialog functionality and style to ImageList component

--- workspaces/web/src/component/image-list/component.tsx
+++ workspaces/web/src/component/image-list/component.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 interface ImageListProps {
   imageURLs: string[];
 }
 
 export function ImageList({ imageURLs }: ImageListProps) {
+  const [activeKey, setActiveKey] = useState<string>("");
+
   return (
     <div className="image list">
+      {activeKey === "" ? null : (
+        <dialog open={true} onClick={() => setActiveKey("")}>
+          <img src={activeKey} />
+        </dialog>
+      )}
       {imageURLs.map((imageURL) => {
         return (
-          <figure key={imageURL}>
+          <figure
+            key={imageURL}
+            onClick={() => setActiveKey(activeKey === imageURL ? "" : imageURL)}
+          >
             <img src={imageURL} />
           </figure>
         );
